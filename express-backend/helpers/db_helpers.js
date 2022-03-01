8126b0dc32a51904ebb50724d3946237ea5a99b0
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-03-01 21:05:30 +0000

    add helpers folder and getInfo db helper

---
+++ express-backend/helpers/db_helpers.js
@@ -0,0 +1,16 @@
+module.exports = () => {
+
+  const getInfo = () => {
+    const query = {
+      text: 'SELECT * FROM info'
+    }
+    return db
+      .query(query)
+      .then(result => result.rows)
+      .catch(err => err)
+  }
+
+  return {
+    getInfo
+  }
+}
